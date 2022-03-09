79f096a503caa441729940a9a8f3b921de492586
Jack Lund <jacklund920@gmail.com>
2022-03-09 11:30:53 -0600

    questions 7  completed in parts-supply.js

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -31,11 +31,10 @@ supplyChanges.push(25);
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 for (let x = 0; x < supplyChanges.length; x++){
-    if (supplyChanges[x] > 0) {
+      if (supplyChanges[x] > 0) {
         console.log(`Added ${supplyChanges[x]} parts.`);
     } if (supplyChanges[x] < 0) {
-        supplyChanges[x] = Math.abs(supplyChanges[x]);
-        console.log(`Removed ${supplyChanges[x]} parts.`);
+        console.log(`Removed ${Math.abs(supplyChanges[x])} parts.`);
     } if (supplyChanges[x] === 0) {
         console.log('No change.');
     }
@@ -43,9 +42,17 @@ for (let x = 0; x < supplyChanges.length; x++){
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for (const x of supplyChanges) {
+      if (x > 0) {
+        console.log(`Added ${x} parts.`);
+    } if (x < 0) {
+        console.log(`Removed ${Math.abs(x)} parts.`);
+    } if (x === 0) {
+        console.log('No change.');
+    }
+} 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
